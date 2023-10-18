353d15946b6798d63039aadf2177f04d3fe23b41
Garrison LeRock <124469216+gar-rock@users.noreply.github.com>
2023-10-18 17:16:18 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,6 +3,8 @@ import string
 
 import re
 
+import time
+
 # Define the character classes
 lowercase_letters = string.ascii_lowercase
 digits = string.digits
@@ -26,12 +28,24 @@ rates["ten_zero"] = 0
 
 anomalies = []
 
-for x in range(0,10000):
+endpoints = []
+total_iter = 10000000
+
+file_name = "api_gateway_endpoints.txt"
+
+time_start = time.time()
+
+
+for x in range(0,total_iter):
+
 
     # Generate a 10-character string
     password = ''.join(random.choice(character_classes) for _ in range(10))
 
     print(password)
+    # this takes too much time
+    #if password not in endpoints:
+    #    endpoints.append(password)
 
     # Check if the generated string matches the regex
     regex_pattern = r"^(?=(?:.*[a-z]){0})(?=(?:.*[0-9]){10})[a-z0-9]{10}$"
@@ -53,7 +67,6 @@ for x in range(0,10000):
     if re.match(regex_pattern, password):
         rates["three_seven"] = rates["three_seven"] + 1
 
-    # Check if the generated string matches the regex
     regex_pattern = r"^(?=(?:.*[a-z]){4})(?=(?:.*[0-9]){6})[a-z0-9]{10}$"
     if re.match(regex_pattern, password):
         rates["four_six"] = rates["four_six"] + 1
@@ -69,11 +82,12 @@ for x in range(0,10000):
     regex_pattern = r"^(?=(?:.*[a-z]){7})(?=(?:.*[0-9]){3})[a-z0-9]{10}$"
     if re.match(regex_pattern, password):
         rates["seven_three"] = rates["seven_three"] + 1
+        endpoints.append(password)
     
-    # Check if the generated string matches the regex
     regex_pattern = r"^(?=(?:.*[a-z]){8})(?=(?:.*[0-9]){2})[a-z0-9]{10}$"
     if re.match(regex_pattern, password):
         rates["eight_two"] = rates["eight_two"] + 1
+        endpoints.append(password)
 
     regex_pattern = r"^(?=(?:.*[a-z]){9})(?=(?:.*[0-9]){1})[a-z0-9]{10}$"
     if re.match(regex_pattern, password):
@@ -87,16 +101,29 @@ for x in range(0,10000):
 
 
 
-print(str(rates["zero_ten"]/100)+ '% are rates["zero_ten"]')
-print(str(rates["one_nine"]/100)+ '% are rates["one_nine"]')
-print(str(rates["two_eight"]/100)+ '% are rates["two_eight"]')
-print(str(rates["three_seven"]/100)+ '% are rates["three_seven"]')
-print(str(rates["four_six"]/100)+ '% are rates["four_six"]')
-print(str(rates["five_five"]/100)+ '% are rates["five_five"]')
-print(str(rates["six_four"]/100)+ '% are rates["six_four"]')
-print(str(rates["seven_three"]/100)+ '% are rates["seven_three"]')
-print(str(rates["eight_two"]/100)+ '% are rates["eight_two"]')
-print(str(rates["nine_one"]/100)+ '% are rates["nine_one"]')
-print(str(rates["ten_zero"]/100)+ '% are rates["ten_zero"]')
+print(str(rates["zero_ten"]/1000)+ '% are rates["zero_ten"]')
+print(str(rates["one_nine"]/1000)+ '% are rates["one_nine"]')
+print(str(rates["two_eight"]/1000)+ '% are rates["two_eight"]')
+print(str(rates["three_seven"]/1000)+ '% are rates["three_seven"]')
+print(str(rates["four_six"]/1000)+ '% are rates["four_six"]')
+print(str(rates["five_five"]/1000)+ '% are rates["five_five"]')
+print(str(rates["six_four"]/1000)+ '% are rates["six_four"]')
+print(str(rates["seven_three"]/1000)+ '% are rates["seven_three"] : Added to the endpoints saved')
+print(str(rates["eight_two"]/1000)+ '% are rates["eight_two"] : Added to the endpoints saved')
+print(str(rates["nine_one"]/1000)+ '% are rates["nine_one"]')
+print(str(rates["ten_zero"]/1000)+ '% are rates["ten_zero"]')
 
 print(anomalies)
+print(str(total_iter) + "total endpoints generated")
+print(str(len(endpoints)) + "total endpoints saved")
+
+
+time_end = time.time()
+
+
+file1 = open(file_name,"w+")
+file1.write("\n".join(endpoints))
+file1.close()
+
+
+print("Total time spent: " + str(time_end-time_start) + " seconds")
